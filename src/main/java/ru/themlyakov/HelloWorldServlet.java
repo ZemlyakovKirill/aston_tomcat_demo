0d22c28ca949772bf7cd9daa0fc7fd0864b10bdd
themlyakov <themlyakov@mail.ru>
2024-05-25 23:14:40 +0300

    initial commit

---
+++ src/main/java/ru/themlyakov/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package ru.themlyakov;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        resp.setStatus(200);
+        try(PrintWriter writer = resp.getWriter()) {
+            writer.write("Hello world test");
+        } catch (IOException e){
+            resp.setStatus(500);
+        }
+    }
+}
