fde50b3d9fdd31de469a1b48b55b6437f002db19
themlyakov <themlyakov@mail.ru>
2024-05-27 18:14:02 +0300

    Test 2

--- src/main/java/ru/themlyakov/HelloWorldServlet.java
+++ src/main/java/ru/themlyakov/HelloWorldServlet.java
@@ -13,7 +13,7 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         resp.setStatus(200);
         try(PrintWriter writer = resp.getWriter()) {
-            writer.write("Hello world test 2");
+            writer.write("Hello world test 3");
         } catch (IOException e){
             resp.setStatus(500);
         }
